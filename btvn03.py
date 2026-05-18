061a6b2a2c2fab6b85657359bcbecf1fb640c595
thidz24 <142291465+thidz24@users.noreply.github.com>
2026-05-18 15:44:35 +0700

    Add files via upload

---
+++ btvn03.py
@@ -0,0 +1,7 @@
+# Nhập dữ liệu
+name_patient = input("Nhập Họ và Tên của bệnh nhân: ")
+disease_id = input("Nhập mã bệnh án:")
+examination_department = input("Nhập khoa/phòng khám chỉ định: ")
+
+# In phiếu
+print(f"Bệnh nhân: {name_patient} - Mã Ba: {disease_id} - chuyển tới {examination_department}")
