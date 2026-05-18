351c2c10ae075c32eda3cd88e644df7af76fcc30
thidz24 <142291465+thidz24@users.noreply.github.com>
2026-05-19 00:22:23 +0700

    Add files via upload

--- btvn03.py
+++ btvn03.py
@@ -1,7 +1,11 @@
+import random
 # Nhập dữ liệu
-name_patient = input("Nhập Họ và Tên của bệnh nhân: ")
-disease_id = input("Nhập mã bệnh án:")
-examination_department = input("Nhập khoa/phòng khám chỉ định: ")
+name_patient = input("Nhập Họ và Tên của bệnh nhân: ") # Nhập họ và tên thì sẽ sử dụng kiểu dữ liệu char
+disease_id = f"BN{random.randint(100,999)}" # id bệnh nhân sẽ để tự động bằng cách sử dụng random
+examination_department = input("Nhập khoa/phòng khám chỉ định: ") # Nhập đại chỉ khoa cũng sử dụng kiểu dữ liệu char
 
 # In phiếu
-print(f"Bệnh nhân: {name_patient} - Mã Ba: {disease_id} - chuyển tới {examination_department}")
+print("---PHIẾU KHÁM BỆNH---")
+print("Họ và Tên:",name_patient)
+print("Mã BA:",disease_id)
+print("Khoa/Phòng khám chỉ định:",examination_department)
